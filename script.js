1324b909d9bdcb6d3523095f79d4320dae432420
Marque Ray <mray2k4@yahoo.com>
2023-06-06 20:40:55 -0700

    Made numbers appear in number box

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
-function calc() {
-    let itemInput = document.getElementById('itemInput').value
-}
+//const item7 = document.getElementById('item7');
+//const itemInput = document.getElementById('itemInput');
+
+function zero() {
+    itemInput.value = item0.value;
+  }
+  item0.addEventListener('click', zero);
+  
+  function one() {
+    itemInput.value = item1.value;
+  }
+  item1.addEventListener('click', one);
+  
+  function two() {
+    itemInput.value = item2.value;
+  }
+  item2.addEventListener('click', two);
+  
+  function three() {
+    itemInput.value = item3.value;
+  }
+  item3.addEventListener('click', three);
+  
+  function four() {
+    itemInput.value = item4.value;
+  }
+  item4.addEventListener('click', four);
+  
+  function five() {
+    itemInput.value = item5.value;
+  }
+  item5.addEventListener('click', five);
+  
+  function five() {
+    itemInput.value = item5.value;
+  }
+  item5.addEventListener('click', five);
+  
+  function six() {
+    itemInput.value = item6.value;
+  }
+  item6.addEventListener('click', six);
+  
+  function seven() {
+    itemInput.value = item7.value;
+  }
+  item7.addEventListener('click', seven);
+  
+  function eight() {
+    itemInput.value = item8.value;
+  }
+  item8.addEventListener('click', eight);
+  
+  function nine() {
+    itemInput.value = item9.value;
+  }
+  item9.addEventListener('click', nine);
+  
