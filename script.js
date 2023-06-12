4ca320ac16331dfbbfd839b2051fba57e4211191
Marque Ray <mray2k4@yahoo.com>
2023-06-12 10:41:11 -0700

    made input and operators output in text box

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
-//const item7 = document.getElementById('item7');
-//const itemInput = document.getElementById('itemInput');
+/* First Input*/
+const firstInput = document.querySelectorAll('#item');
 
-function zero() {
-    itemInput.value = item0.value;
+firstInput.forEach(function(e) {
+  e.addEventListener('click', func);
+})
+function func(e) {
+  input.value += e.target.textContent;
+}
+
+
+/* Operator Select */
+const oper = document.querySelectorAll('#oper');
+
+oper.forEach(function(e) {
+  e.addEventListener('click', func)
+})
+function func(e) {
+  input.value += e.target.textContent;
+}
+
+
+/* Second Input */
+const secondInput = document.querySelectorAll('#item');
+
+firstInput.forEach(function(e) {
+  e.addEventListener('click', func);
+})
+function func(e) {
+  input.value += e.target.textContent;
+}
+
+
+/* Results */
+const equal = document.getElementById('equal');
+equal.addEventListener('click', results)
+ 
+function results() {
+  switch(oper) {
+    case '*':
+      console.log('firstInput');
+      break
+    default:
+      console.log('no Input')
   }
-  item0.addEventListener('click', zero);
-  
-  function one() {
-    itemInput.value = item1.value;
-  }
-  item1.addEventListener('click', one);
-  
-  function two() {
-    itemInput.value = item2.value;
-  }
-  item2.addEventListener('click', two);
-  
-  function three() {
-    itemInput.value = item3.value;
-  }
-  item3.addEventListener('click', three);
-  
-  function four() {
-    itemInput.value = item4.value;
-  }
-  item4.addEventListener('click', four);
-  
-  function five() {
-    itemInput.value = item5.value;
-  }
-  item5.addEventListener('click', five);
-  
-  function five() {
-    itemInput.value = item5.value;
-  }
-  item5.addEventListener('click', five);
-  
-  function six() {
-    itemInput.value = item6.value;
-  }
-  item6.addEventListener('click', six);
-  
-  function seven() {
-    itemInput.value = item7.value;
-  }
-  item7.addEventListener('click', seven);
-  
-  function eight() {
-    itemInput.value = item8.value;
-  }
-  item8.addEventListener('click', eight);
-  
-  function nine() {
-    itemInput.value = item9.value;
-  }
-  item9.addEventListener('click', nine);
-  
+}
+
+
+/* Clear */
+Sconst clear = document.getElementById('clear');
+clear.addEventListener('click', reset);
+       
+function reset() {
+  document.getElementById('input').value = '';
+}
