95d09591be10eec71e2bd2a4c2341abd0b00d45c
Marque Ray <mray2k4@yahoo.com>
2023-06-19 10:33:21 -0700

    added number length, still need to adjust

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-/* First Input*/
+/* Inputs */
 const firstInput = document.querySelectorAll('#item');
+
+let maxCount = 10
+const counter = document.getElementById('counter');;
+const items = document.getElementById('item');
 
 firstInput.forEach(function(e) {
   e.addEventListener('click', func1);
 })
 function func1(e) {
   input.value += e.target.textContent;
+  if (maxCount == 0 && e.button !== 'Backspace' || maxCount == 10 && e.button == 'Backspace') {
+  e.preventDefault();
+      return;
 }
+    if(e.button == 'Backspace') {
+      maxCount = maxCount + 1;
+      counter.innerHTML = maxCount;
+    } else {
+      maxCount = maxCount - 1;
+  counter.innerHTML = maxCount;
+    }
+}
+
 
 
 /* Operator Select */
@@ -20,6 +36,7 @@ function oper1(e) {
   }
 
 
+/* Results */
 const equal = document.getElementById('equal');
 equal.addEventListener('click', results)
 
@@ -28,9 +45,9 @@ const Calc = function(c) {
   return new Function('return ' + c)();
 }
 
-input.value = parseFloat(Calc(input.value).toFixed(3));
+input.value = parseFloat(Calc(input.value).toFixed(3)); 
 }
-input.value = '';
+
 
 
 /* Clear */
@@ -38,5 +55,5 @@ const clear = document.getElementById('clear');
 clear.addEventListener('click', reset);
        
 function reset() {
-  document.getElementById('input').value = '';
+  document.getElementById('input').value= '';
 }
