1a334f08f6ab725dd3557d6aee1ddd3d2bf33e14
Mray2k4 <99221965+Mray2k4@users.noreply.github.com>
2023-06-27 15:41:45 -0700

    add the 3 decimal placements

--- script.js
+++ script.js
@@ -1,96 +1,86 @@
-let firstInput = document.querySelectorAll('#item');
-let operator = document.querySelectorAll('#oper');
-//let decimal = document.getElementById('decimal');
-let input = document.getElementById('input');
+let firstInput = document.querySelectorAll("#item");
+let operator = document.querySelectorAll("#oper");
+let input = document.getElementById("input");
 
-
-//input.value = '0';
 let num1;
 let num2;
 let oper;
 
-
 /* First Input*/
-firstInput.forEach(function(e) {
-  e.addEventListener('click', func1);
-})
+firstInput.forEach(function (e) {
+  e.addEventListener("click", func1);
+});
 function func1(num) {
   let numbers = num.target.value;
-    if (input.value === '0') 
-    input.value = '';
-    input.value += numbers;
-    input.textContent = input.value;
-  
+  if (input.value === "0") input.value = "";
+  input.value += numbers;
+  input.textContent = input.value;
+
   if (!num1) {
     num1 = numbers;
-} else  if (!!num1 && !oper) {
+  } else if (!!num1 && !oper) {
     num1 += numbers;
-} else if (!num2) {
+  } else if (!num2) {
     num2 = numbers;
-} else if (!!num1 && !!oper) {
+  } else if (!!num1 && !!oper) {
     num2 += numbers;
-} else {
-}
+  } else {
+  }
 }
 
 /* Operator Select */
-operator.forEach(function(e) {
-  e.addEventListener('click', oper1)
-})
+operator.forEach(function (e) {
+  e.addEventListener("click", oper1);
+});
 function oper1(e) {
- oper = e.target.value;
-    input.value = '';
-  
-}
-
-/* Decimal */
-const decimal = document.getElementById('decimal');
-decimal.addEventListener('click', dec)
-
-function dec() {
-  if (!input.value.includes('.'))
-    input.value += '.';
+  results();
+  oper = e.target.value;
+  input.value = "";
 }
 
 /* Results */
-const equal = document.getElementById('equal');
-equal.addEventListener('click', results)
+const equal = document.getElementById("equal");
+equal.addEventListener("click", results);
 
 function results() {
-  if (oper === '+') {
+  if (oper === "+") {
     num1 = parseInt(num1) + parseInt(num2);
     input.value = num1;
-    num2 = '';
-    } 
-  if (oper === '-') {
+    num2 = "";
+    decimalPlacement();
+  }
+  if (oper === "-") {
     num1 = num1 - num2;
     input.value = num1;
-    num2 = '';
+    num2 = "";
+    decimalPlacement();
   }
-  if (oper === '*') {
+  if (oper === "*") {
     num1 = num1 * num2;
     input.value = num1;
-    num2 = '';
-  } 
-  if (oper === '/') {
+    num2 = "";
+    decimalPlacement();
+  }
+  if (oper === "/") {
     num1 = num1 / num2;
     input.value = num1;
-    num2 = '';
-} else {
-  
-//input.value = parseFloat(results(input.value).toFixed(3));
-}
+    num2 = "";
+    decimalPlacement();
+  }
 }
 
-
+/* 3 Decimal Placement */
+function decimalPlacement() {
+  input.value = parseFloat(num1.toFixed(3));
+}
 
 /* Clear */
-const clear = document.getElementById('clear');
-clear.addEventListener('click', reset);
-       
+const clear = document.getElementById("clear");
+clear.addEventListener("click", reset);
+
 function reset(num) {
-  input.value = '0';
-  num1 = '';
-  num2 = '';
-  oper = '';
-};
+  input.value = "0";
+  num1 = "";
+  num2 = "";
+  oper = "";
+}
